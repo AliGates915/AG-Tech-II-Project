19fc4fca1b81b10b682171a5d3da1d1275f99a22
Viraj Gavade <vrajgavade17@gmail.com>
2025-05-24 16:45:28 +0530

    `Added NotFound page and route to catch unmatched routes in App.jsx`

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,6 +12,7 @@ import CourseBook from "./Pages/CourseBook";
 import TechGuidance from "./Pages/TechGuidance";
 import ContactUs from "./Pages/ContactUs";
 import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
 
 import Login from "./components/Login";
 import SignUpPage from "./components/SignUpPage";
@@ -39,6 +40,8 @@ const Layout = () => {
         <Route path="/course-book" element={<CourseBook />} />
         <Route path="/tech-guidance" element={<TechGuidance />} />
         <Route path="/contact-us" element={<ContactUs />} />
+        {/* 404 Page - This will catch all unmatched routes */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
       {shouldShowFooter && <Footer />}
     </>
